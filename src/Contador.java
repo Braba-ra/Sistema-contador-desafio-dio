314ebf7cd253d67195723462cbb0117231c9715f
Bárbara Letícia Soares <barbaracavalcanti665@gmail.com>
2024-08-23 09:30:09 -0300

    Contador

--- src/Contador.java
+++ src/Contador.java
@@ -4,9 +4,9 @@ public class Contador {
 	public static void main(String[] args) {
 		@SuppressWarnings("resource")
         Scanner terminal = new Scanner(System.in);
-		System.out.println("Digite o primeiro parâmetro");
+		System.out.println("Digite o primeiro parâmetro: ");
 		int parametroUm = terminal.nextInt();
-		System.out.println("Digite o segundo parâmetro");
+		System.out.println("Digite o segundo parâmetro: ");
 		int parametroDois = terminal.nextInt();
 		
 		try {
